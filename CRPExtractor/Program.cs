3d9fa4a8232d9d77f8d76de65ffbf32b787fefa9
clausthaler-devel <claus.thaler.devel@gmail.com>
2016-07-03 21:56:46 +0200

    Moved command line tool into separate project

---
+++ CRPExtractor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CRPTools;
+using CommandLine;
+
+
+namespace CRPExtractor
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var options = new Options();
+
+            if ( args.Length == 1 )
+            {
+                options.InputFile = args[0];
+                options.Verbose = false;
+                options.SaveFiles = true;
+
+                CrpDeserializer deserializer = new CrpDeserializer(options.InputFile);
+                deserializer.parseFile( options );
+            }
+            else if ( Parser.Default.ParseArguments( args, options ) )
+            {
+                CrpDeserializer deserializer = new CrpDeserializer(options.InputFile);
+                deserializer.parseFile( options );
+            }
+        }
+    }
+}
---
+++ CRPParser/Models/CrpAssetInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ImageMagick;
+
+namespace CRPTools
+{
+    public class CrpAssetInfo
+    {
+        public CrpHeader header { get; set; }
+        public Dictionary<string, MaterialStub> Materials = new Dictionary<string, MaterialStub>();
+        public Dictionary<string, MagickImage> Images = new Dictionary<string, MagickImage>();
+        public Dictionary<string, MagickImage> Textures = new Dictionary<string, MagickImage>();
+        public Dictionary<string, Mesh> Meshes = new Dictionary<string, Mesh>();
+        public Dictionary<string, Dictionary<string, dynamic>> InfoGens = new Dictionary<string, Dictionary<string, dynamic>>();
+        public Dictionary<string, dynamic> gameObjects = new Dictionary<string, dynamic>();
+        public Dictionary<string, dynamic> metadata;
+    }
+}
