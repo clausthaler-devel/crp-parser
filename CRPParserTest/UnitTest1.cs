c62c89d3c30dfcf806e9bb96c3c19a70a297cb28
clausthaler-devel <claus.thaler.devel@gmail.com>
2016-07-03 21:57:26 +0200

    added Testing skeleton

---
+++ CRPParserTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using CRPTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CRPParserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {   
+            var cwd = Directory.GetCurrentDirectory();
+            var path = Path.Combine(cwd, "..", "..", "testfiles", "Colossal Mills.crp");
+            var info = new CrpDeserializer(path).parseFile();
+            Assert.AreEqual( info.metadata["name"], "Colossal Mills" );
+        }
+    }
+}
